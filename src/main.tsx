5341c7d596300fbd789e9f343c9c9cc199fe94a0
woodylucas <wlucas0311@gmail.com>
2025-09-16 19:48:15 -0400

    feat: added files

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./App.css";
+
+const element = document.getElementById("root")!;
+
+const root = ReactDOM.createRoot(element);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
